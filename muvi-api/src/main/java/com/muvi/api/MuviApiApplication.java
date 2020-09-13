2040d3d189cd888bc3b92556764e9ce357a33c65
felipejsm <felipejsm41@gmail.com>
2020-09-13 16:03:07 -0300

    Spring Boot Web Initializr skeleton

---
+++ muvi-api/src/main/java/com/muvi/api/MuviApiApplication.java
@@ -0,0 +1,13 @@
+package com.muvi.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuviApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuviApiApplication.class, args);
+	}
+
+}
---
+++ muvi-api/src/test/java/com/muvi/api/MuviApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muvi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuviApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
