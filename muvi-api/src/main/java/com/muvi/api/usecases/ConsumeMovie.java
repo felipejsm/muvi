4e128ac8be222fddab1892572a50e7b72f4bb3a9
felipejsm <felipejsm41@gmail.com>
2020-09-15 22:02:02 -0300

    Implementing kafka's consumer and producer

--- muvi-api/src/main/java/com/muvi/api/usecases/ConsumeMovie.java
+++ muvi-api/src/main/java/com/muvi/api/usecases/ConsumeMovie.java
@@ -1,4 +1,12 @@
 package com.muvi.api.usecases;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ConsumeMovie {
+    @KafkaListener(topics = "${movie.topic}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consumer(String order) {
+        System.out.println("Order: "+order);
+    }
 }
--- muvi-api/src/main/java/com/muvi/api/usecases/ProduceMovie.java
+++ muvi-api/src/main/java/com/muvi/api/usecases/ProduceMovie.java
@@ -1,4 +1,24 @@
 package com.muvi.api.usecases;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.UUID;
+
+@Component
 public class ProduceMovie {
+    @Value("${movie.topic}")
+    private String movieTopic;
+    private KafkaTemplate kafkaTemplate;
+
+    public ProduceMovie(final KafkaTemplate kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void send(final @RequestBody String movie) {
+        final String messageKey = UUID.randomUUID().toString();
+        kafkaTemplate.send(movieTopic, messageKey, movie);
+    }
 }
